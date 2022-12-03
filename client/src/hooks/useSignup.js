9eff01541c44ce9036eeba3aba7612a61077681d
micheci <michecimartinez@gmail.com>
2022-12-02 23:59:55 -0600

    a

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -11,7 +11,7 @@ export const useSignup=()=>{
         setIsLoading(true)
         setError(null)
 
-        const resposne=await fetch('/user/signup',{
+        const resposne=await fetch('https://doublestreamback.onrender.com/user/signup',{
             method:'POST',
             headers:{'Content-Type':'application/json'},
             body:JSON.stringify({userName,password})
